00d6dc0ad9e58c085156622b4dc2ead886a01a12
Nitin Kumar <2018kumarnitin@gmail.com>
2023-07-23 18:46:48 +0530

    formatted the code

--- src/contact/models/contact.dto.ts
+++ src/contact/models/contact.dto.ts
@@ -1,10 +1,4 @@
-import { ValidatorOptions } from '@nestjs/common/interfaces/external/validator-options.interface';
-import {
-  IsEmail,
-  IsMobilePhone,
-  IsNotEmpty,
-  ValidateIf,
-} from 'class-validator';
+import { IsEmail, IsMobilePhone, ValidateIf } from 'class-validator';
 
 export class Contact {
   @ValidateIf((object: any, value: any) => value !== null)
